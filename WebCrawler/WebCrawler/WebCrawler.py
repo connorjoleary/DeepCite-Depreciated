ae931dfe0131adee6e4d60f0eaf6ce72b86883a5
deadmau6 <joepop16@gmail.com>
2017-02-28 22:17:58 -0700

    Uses custom html parser to create a single object containing an organized structure of the html  tags and their associated data.

--- WebCrawler/WebCrawler/WebCrawler.py
+++ WebCrawler/WebCrawler/WebCrawler.py
@@ -1,25 +1,34 @@
 import urllib.request
 from html.parser import HTMLParser
 
+#The dictionary below contains all of the html tags and their associated data
+#in the following (key : pair) format. ((tag name, occurance number) : data) 
+#Currently this dictionary does not contain the Attributes of the tags
+htmlPage = dict()
+
+#This is a list of tuples (tag, occurance), which are the keys of htmlPage
+tagList = list()
+
 class MyHTMLParser(HTMLParser):
 
-    def handle_startendtag(self, tag, attrs):
-        print("Start Tag : ", tag)
-        for a in attrs:
-            try:
-                print(" Attr : ", a)
-            except UnicodeEncodeError:
-                print(" attr    ")
-
-    def handle_endtag(self, tag):
-        print("End Tag : ", tag)
+    def handle_starttag(self, tag, attrs):
+        #add tuple key to list, use len(tagList) + 1 to avoid NoneType error 
+        tagList.append((tag,len(tagList)+1))
 
     def handle_data(self, data):
-        try:
-            print("Data : ", data)
-        except UnicodeEncodeError:
-            print(" data    ")
+        #check if there are tags and valid data to add
+        if data and tagList:
+            #add the current data to the current starttag
+            htmlPage[tagList[-1]] = data
+
+    def handle_comment(self, data):
+        #similarly this adds comments to the dictionary
+        if data and tagList:
+            commentKey = ("Comment", tagList[-1][1])
+            htmlPage[commentKey] = data
 
 parser = MyHTMLParser()
 with urllib.request.urlopen('https://academic.oup.com/nar/article/38/suppl_2/W214/1126704/The-GeneMANIA-prediction-server-biological-network#20150589') as f:
     parser.feed(f.read().decode('utf-8'))
+print(tagList)
+#print(article) currently can't print because of minor Unicode Error
