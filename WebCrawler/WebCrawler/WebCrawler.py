eb557a470a2fbecbb2ab1a1fe06077751707667c
deadmau6 <joepop16@gmail.com>
2017-03-03 23:20:06 -0700

    can now associate the links to the text.

--- WebCrawler/WebCrawler/WebCrawler.py
+++ WebCrawler/WebCrawler/WebCrawler.py
@@ -7,6 +7,8 @@ from html.parser import HTMLParser
 #Currently this dictionary does not contain the Attributes of the tags
 htmlPage = dict()
 
+links = dict()
+
 #This is a list of tuples (tag, occurance), which are the keys of htmlPage
 tagList = list()
 
@@ -15,14 +17,14 @@ class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         #add tuple key to list, use len(tagList) + 1 to avoid NoneType error 
         tagList.append((tag,len(tagList)+1))
+        for a in attrs:
+            if re.search(r'href', a[0]):
+                links[tagList[-1]] = a
 
     def handle_data(self, data):
         #check if there are tags and valid data to add
-        try:
-            if re.search(r'\w+', data):
-                print(data)
-        except UnicodeEncodeError:
-            print("*************")
+        if re.search(r'\w+', data) and tagList:
+            htmlPage[tagList[-1]] = data
 
     def handle_comment(self, data):
         #similarly this adds comments to the dictionary
@@ -33,3 +35,8 @@ class MyHTMLParser(HTMLParser):
 parser = MyHTMLParser()
 with urllib.request.urlopen('https://academic.oup.com/nar/article/38/suppl_2/W214/1126704/The-GeneMANIA-prediction-server-biological-network#20150589') as f:
     parser.feed(f.read().decode('utf-8'))
+
+for h in htmlPage.keys():
+    for l in links.keys():
+        if h == l:
+            print(h, l)
