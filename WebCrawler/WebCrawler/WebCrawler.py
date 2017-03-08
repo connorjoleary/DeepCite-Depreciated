3fc9c89acb319212416da2a3cd84fc1df58833e9
deadmau6 <joepop16@gmail.com>
2017-03-08 14:18:31 -0700

    Cleaned up and now everything works smoothly. Look at page Comments for details.

--- WebCrawler/WebCrawler/WebCrawler.py
+++ WebCrawler/WebCrawler/WebCrawler.py
@@ -3,40 +3,44 @@ import re
 from html.parser import HTMLParser
 
 #The dictionary below contains all of the html tags and their associated data
-#in the following (key : pair) format. ((tag name, occurance number) : data) 
+#in the following (key : pair) format. (pageIndex : data) 
 #Currently this dictionary does not contain the Attributes of the tags
 htmlPage = dict()
 
+#Ditionary containing (pageIndex : hyperlink)
 links = dict()
-
-#This is a list of tuples (tag, occurance), which are the keys of htmlPage
-tagList = list()
 
 class MyHTMLParser(HTMLParser):
 
+    #This is the global index for htmlPage and links
+    pageIndex = 0
+
     def handle_starttag(self, tag, attrs):
-        #add tuple key to list, use len(tagList) + 1 to avoid NoneType error 
-        tagList.append((tag,len(tagList)+1))
+        #increment the page index for every tag seen 
+        self.pageIndex += 1
         for a in attrs:
             if re.search(r'href', a[0]):
-                links[tagList[-1]] = a
+                links[self.pageIndex] = a
 
     def handle_data(self, data):
         #check if there are tags and valid data to add
-        if re.search(r'\w+', data) and tagList:
-            htmlPage[tagList[-1]] = data
+        if re.search(r'\w+', data) and self.pageIndex:
+            htmlPage[self.pageIndex] = data
 
     def handle_comment(self, data):
         #similarly this adds comments to the dictionary
-        if data and tagList:
-            commentKey = ("Comment", tagList[-1][1])
-            htmlPage[commentKey] = data
+        if data and self.pageIndex:
+            htmlPage[self.pageIndex] = data
 
 parser = MyHTMLParser()
 with urllib.request.urlopen('https://academic.oup.com/nar/article/38/suppl_2/W214/1126704/The-GeneMANIA-prediction-server-biological-network#20150589') as f:
     parser.feed(f.read().decode('utf-8'))
 
-for h in htmlPage.keys():
-    for l in links.keys():
+#prints out the link item associated with a htmlPage data
+for h, v in htmlPage.items():
+    for l, d in links.items():
         if h == l:
-            print(h, l)
+            try:
+                print(v, d)
+            except UnicodeEncodeError:
+                print(h, l)
