83857ddb19fb92ef7413c0b86cf319b65968b71a
fippy24 <connor.trumpet@gmail.com>
2017-05-25 15:04:53 -0600

    Dumb citations

---
+++ Citations.py
@@ -0,0 +1,10 @@
+#This will use regex to create an object compsed of possible citations
+import re
+from WebCrawler import *
+
+generalMLA = re.compile(r'[A-Za-z ]+\.[A-Za-z ]+\..+,')
+
+for x,y in htmlPage.items():
+    try:
+        print(y)
+    except UnicodeEncodeError:
