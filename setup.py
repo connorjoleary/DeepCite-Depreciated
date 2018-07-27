c6d0565d1723d73ab0deadf094ae451dc5d08cfb
Connor O'Leary <oleary.connor24@gmail.com>
2018-07-26 23:02:39 -0600

    Setup file structure

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='DeepCite',
+    version='0.1.0',
+    author='Connor OLeary',
+    author_email='oleary.connor24@gmail.com',
+    packages=['deepcite'],
+    url='https://github.com/connorjoleary/DeepCite',
+    license='LICENSE.txt',
+    description='Finding the original source of text.',
+    long_description=open('README.md').read(),
+    install_requires=[],
+)
